cf0221e542aee1a6d512908e7bf5709dffd2e6a9
SonnySon1 <sonysudrajat94@gmail.com>
2025-05-16 04:13:18 +0700

    feat(seeders): menambahkan seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Menu;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        User::create([
+            'name' => 'Alberto',
+            'username' => 'alberto',
+            'email' => 'alberto@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+        User::create([
+            'name' => 'Alexander',
+            'username' => 'alexander',
+            'email' => 'alexander@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+
     }
 }
