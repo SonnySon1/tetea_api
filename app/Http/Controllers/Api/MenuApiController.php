66205230c1ca3d71b37fbd3bbdf7a017b956cdfb
SonnySon1 <sonysudrajat94@gmail.com>
2025-05-17 03:32:39 +0700

    feat(app): menambahkan validate ValidationException untuk crud menu

--- app/Http/Controllers/Api/MenuApiController.php
+++ app/Http/Controllers/Api/MenuApiController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Helpers\ResponseFormater;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class MenuApiController extends Controller
 {
@@ -23,25 +24,31 @@ class MenuApiController extends Controller
 
     // store data
     public function store(Request $request) {
-        $request->validate([
-            'name'        => 'required',
-            'price'       => 'required',
-            'description' => 'required'
-        ]);
+        try {
+            $request->validate([
+                'name'        => 'required',
+                'price'       => 'required',
+                'description' => 'required'
+            ]);
 
-        $data = [
-            'name'        => $request->name,
-            'price'       => $request->price,
-            'description' => $request->description,
-            'user_id'     => $request->user_id
-        ];
+            $data = [
+                'name'        => $request->name,
+                'price'       => $request->price,
+                'description' => $request->description,
+                'user_id'     => $request->user_id
+            ];
 
-        $data_menu = Menu::create($data);
+            $data_menu = Menu::create($data);
 
-        if ($data_menu) {
-            return ResponseFormater::success($data_menu, 'Store data menu success', 200);
-        } else {
-            return ResponseFormater::error('Store data menu failed', 500);
+            if ($data_menu != null) {
+                return ResponseFormater::success($data_menu, 'Store data menu success', 200);
+            } 
+            else{
+                return ResponseFormater::error('Store data menu failed', 500);
+            }
+        
+        } catch (ValidationException $e) {
+            return ResponseFormater::error('Validasi gagal', 422, $e->errors());
         }
     }
 
@@ -58,32 +65,36 @@ class MenuApiController extends Controller
 
     // update data
     public function update(Request $request, $id) {
-         $request->validate([
-            'name'        => 'required',
-            'price'       => 'required',
-            'description' => 'required'
-        ]);
+         try {
+            $request->validate([
+                'name'        => 'required',
+                'price'       => 'required',
+                'description' => 'required'
+            ]);
 
-        $data = [
-            'name'        => $request->name,
-            'price'       => $request->price,
-            'description' => $request->description,
-            'user_id'     => $request->user_id
-        ];
+            $data = [
+                'name'        => $request->name,
+                'price'       => $request->price,
+                'description' => $request->description,
+                'user_id'     => $request->user_id
+            ];
 
-        $data_menu = Menu::where('id', $id);
-        $data_menu->update($data);
+            $data_menu = Menu::where('id', $id);
+            $data_menu->update($data);
 
-        if ($data_menu) {
-            return ResponseFormater::success($data_menu, 'Update data menu success', 200);
-        } else {
-            return ResponseFormater::error('Update data menu failed', 500);
-        }
+            if ($data_menu) {
+                return ResponseFormater::success($data_menu, 'Update data menu success', 200);
+            } else {
+                return ResponseFormater::error('Update data menu failed', 500);
+            }
+         } catch (ValidationException $e) {
+            return ResponseFormater::error('Validasi gagal', 422, $e->errors());
+         }
     }  
 
     // delete data
     public function destroy($id) {
-        $data_menu = Menu::where('id', $id);
+        $data_menu = Menu::where('id', $id)->first();
         $data_menu->delete();
 
         if ($data_menu->count() > 0) {
